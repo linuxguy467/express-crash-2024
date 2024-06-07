97926d40b088f380024ff1eb742f867e635a04c0
linuxguy467 <me@matthem.codes>
2024-06-07 19:33:34 -0400

    create about route

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
 app.listen(5010, () => console.log(`server is running on port 5010`));
