022a6d3ed5bc08a0c183fcf740aab62b1307df81
linuxguy467 <me@matthem.codes>
2024-06-08 16:25:11 -0400

    add posts delete request

--- routes/posts.js
+++ routes/posts.js
@@ -68,4 +68,19 @@ router.put('/:id', (req, res) => {
   res.status(200).json({ msg: `Post with id ${id} was successfully updated` });
 });
 
+// Delete one post
+router.delete('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json({ msg: `Post with id ${id} has been deleted` });
+});
+
 export default router;
