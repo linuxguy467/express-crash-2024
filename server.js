d0b546648471ae874cc4e52207ee398ad4e9c473
linuxguy467 <me@matthem.codes>
2024-06-07 20:08:42 -0400

    replace both home and about routes with public middleware

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5010, () => console.log(`server is running on port 5010`));
