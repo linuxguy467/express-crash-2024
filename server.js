188b5f1715f71a80750bf0497c9eb51f91b6b526
linuxguy467 <me@matthem.codes>
2024-06-08 09:22:18 -0400

    set status codes

--- server.js
+++ server.js
@@ -19,21 +19,27 @@ app.get('/api/posts', (req, res) => {
   const sort = req.query.sort;
 
   if (!isNaN(limit) && limit > 0 && sort === 'desc') {
-    res.json([...posts].sort((a, b) => b.id - a.id).slice(0, limit));
+    return res
+      .status(200)
+      .json([...posts].sort((a, b) => b.id - a.id).slice(0, limit));
   } else if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
+    return res.status(200).json(posts.slice(0, limit));
   } else if (sort === 'desc') {
-    res.json([...posts].sort((a, b) => b.id - a.id));
-  } else {
-    res.json(posts);
+    return res.status(200).json([...posts].sort((a, b) => b.id - a.id));
   }
+  res.status(200).json(posts);
 });
 
 app.get('/api/posts/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
-  res.json(post);
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+  res.status(200).json(post);
 });
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
