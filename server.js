51fde01d6984d24e466d5c86acc912374e54f08d
linuxguy467 <me@matthem.codes>
2024-06-07 20:20:34 -0400

    create test json data api route

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const app = express();
 // setup static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+let posts = [
+  { id: 1, title: 'First Post' },
+  { id: 2, title: 'Second Post' },
+  { id: 3, title: 'Third Post' },
+];
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
 app.listen(5010, () => console.log(`server is running on port 5010`));
