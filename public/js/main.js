a1c5feac04ae888ccc34877af1907202b6c5b5e5
linuxguy467 <me@matthem.codes>
2024-06-09 18:13:39 -0400

    add dialog for posts and update functionality on the frontend

--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,70 @@ async function showPosts() {
 
     const posts = await res.json();
 
+    while (output.lastElementChild) {
+      output.removeChild(output.lastElementChild);
+    }
+
     posts.forEach((post) => {
       const postEl = document.createElement('div');
       postEl.textContent = post.title;
+      postEl.addEventListener('click', () => {
+        const dialog = `
+        <dialog id="infoModal-post">
+          <h2>Post Details</h2>
+          <form id="update-post">
+            <label for="post-title">Title:</label>
+            <input type="text" id="post-title" name="title" value="${post.title}"/><br /><br />
+            <button type="submit">Update Post</button>
+          </form>
+        </dialog>
+        `;
+        const dialogContainer = document.createElement('div');
+        dialogContainer.innerHTML = dialog;
+        const dialogEl = dialogContainer.firstElementChild;
+
+        dialogEl.addEventListener('click', (e) => {
+          e.stopPropagation();
+          const dialogDimensions = dialogEl.getBoundingClientRect();
+          if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom
+          ) {
+            dialogEl.close();
+            postEl.removeChild(dialogEl);
+          }
+        });
+
+        postEl.appendChild(dialogEl);
+
+        document
+          .querySelector('#update-post')
+          .addEventListener('submit', async function (e) {
+            e.preventDefault();
+            const formData = new FormData(this);
+            const title = formData.get('title');
+
+            const res = await fetch(`/api/posts/${post.id}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ title }),
+            });
+
+            if (!res.ok) {
+              throw new Error('Failed to update post');
+            }
+
+            dialogEl.close();
+            postEl.removeChild(dialogEl);
+            showPosts();
+          });
+
+        dialogEl.showModal();
+      });
       output.appendChild(postEl);
     });
   } catch (error) {
