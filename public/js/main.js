f10f3741b10a52b28e06da68aa92d1b8574695ac
linuxguy467 <me@matthem.codes>
2024-06-09 13:10:31 -0400

    add create post functionality to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,9 @@
 
 const output = document.querySelector('#output');
 const button = document.querySelector('#get-posts-btn');
+const form = document.querySelector('#add-post-form');
 
+// Get posts
 async function showPosts() {
   try {
     const res = await fetch('/api/posts');
@@ -22,4 +24,36 @@ async function showPosts() {
   }
 }
 
+// Submit new post
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get('title');
+
+  try {
+    const res = await fetch('/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to add post');
+    }
+
+    const newPost = await res.json();
+
+    const postEl = document.createElement('div');
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Event Listeners
 button.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
