b950ad59c0e2107ac4eaca9c158ecb2dfb1d9fa1
linuxguy467 <me@matthem.codes>
2024-06-11 13:42:11 -0400

    add a check on localstorage key for id

--- public/js/post.js
+++ public/js/post.js
@@ -4,28 +4,33 @@ async function main() {
   const postIdField = document.querySelector('#postId');
   const postTitleField = document.querySelector('#postTitle');
 
-  const id = parseInt(localStorage.getItem('clickedPostID'));
+  let id = '';
+  if ('clickedPostID' in localStorage) {
+    id = parseInt(localStorage.getItem('clickedPostID'));
 
-  if (id && !isNaN(id)) {
-    try {
-      postIdField.textContent = ``;
-      postTitleField.textContent = ``;
+    if (id && !isNaN(id)) {
+      try {
+        postIdField.textContent = ``;
+        postTitleField.textContent = ``;
 
-      const res = await fetch(`/api/posts/${id}`);
+        const res = await fetch(`/api/posts/${id}`);
 
-      if (!res.ok) {
-        throw new Error(`Failed to fetch post with id ${id}`);
+        if (!res.ok) {
+          throw new Error(`Failed to fetch post with id ${id}`);
+        }
+
+        const post = await res.json();
+
+        postIdField.textContent = `${post.id}`;
+        postTitleField.textContent = `${post.title}`;
+      } catch (error) {
+        console.error(error);
       }
-
-      const post = await res.json();
-
-      postIdField.textContent = `${post.id}`;
-      postTitleField.textContent = `${post.title}`;
-    } catch (error) {
-      console.error(error);
+    } else {
+      console.error(new Error('The ID is not a number'));
     }
   } else {
-    console.error(new Error('The ID is not a number'));
+    console.error(new Error('The ID does not exist'));
   }
 }
 main();
